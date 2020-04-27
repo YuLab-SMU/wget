c9fa5a0a4b2feb32017ee2dd94be41f5204b6aa7
Guangchuang Yu <guangchuangyu@gmail.com>
2020-04-27 10:41:14 +0800

    add exe url

--- R/zzz.R
+++ R/zzz.R
@@ -1,13 +1,14 @@
 .onAttach <- function(libname, pkgname) {
-  os <- Sys.info()[1]
-  if (os == "Windows") {
-    path <- Sys.getenv('path')
-    wget_path <- system.file("win", package="wget")
-
-    Sys.setenv(path = paste0(wget_path, ';', path))
-  }
-  get_download_setting()
-  wget_set()
+    os <- Sys.info()[1]
+    if (os == "Windows") {
+        ## windows binary file downloaded from https://eternallybored.org/misc/wget/
+        path <- Sys.getenv('path')
+        wget_path <- system.file("win", package="wget")
+        
+        Sys.setenv(path = paste0(wget_path, ';', path))
+    }
+    get_download_setting()
+    wget_set()
 }
 
 
