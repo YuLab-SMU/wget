d004e5d9d115fa32ecf26d15842dde3ba54ba2dc
chaoslure <chaoslure@foxmail.com>
2020-05-09 21:28:59 +0800

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,14 +1,16 @@
 .onAttach <- function(libname, pkgname) {
-    os <- Sys.info()[1]
-    if (os == "Windows") {
-        ## windows binary file downloaded from https://eternallybored.org/misc/wget/
-        path <- Sys.getenv('path')
-        wget_path <- system.file("win", package="wget")
-        
-        Sys.setenv(path = paste0(wget_path, ';', path))
-    }
-    get_download_setting()
-    wget_set()
+  os <- Sys.info()
+  if (os[1] == "Windows") {
+    ## windows binary file downloaded from https://eternallybored.org/misc/wget/
+    path <- Sys.getenv('path')
+    os_b = strsplit(os[2]," ")[[1]][2]
+    if(os_b == "x64"){
+      wget_path <- system.file("win/x64", package="wget")
+    }else{
+      wget_path <- system.file("win/x86", package="wget")
+    } 
+    Sys.setenv(path = paste0(wget_path, ';', path))
+  }
+  get_download_setting()
+  wget_set()
 }
-
-
