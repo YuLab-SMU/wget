7968f457553ce439fd974d0ca6f308ff2ee2565f
chaoslure <chaoslure@foxmail.com>
2020-05-11 08:30:59 +0800

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,9 @@
 .onAttach <- function(libname, pkgname) {
   os <- Sys.info()
-  if (os[1] == "Windows") {
+  if (os["sysname"] == "Windows") {
     ## windows binary file downloaded from https://eternallybored.org/misc/wget/
     path <- Sys.getenv('path')
-    os_b = strsplit(os[2]," ")[[1]][2]
+    os_b = strsplit(os["machine"]," ")[[1]][2]
     if(os_b == "x64"){
       wget_path <- system.file("win/x64", package="wget")
     }else{
