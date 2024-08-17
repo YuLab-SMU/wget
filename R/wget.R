6fc73e1cc428f7e68697f818e1dc73258f833301
Guangchuang Yu <guangchuangyu@gmail.com>
2024-08-17 12:51:18 +0800

    rm the wget.exe

--- R/wget.R
+++ R/wget.R
@@ -48,3 +48,31 @@ get_download_setting <- function() {
            getOption('download.file.extra'),
            envir = .wgetEnv)
 }
+
+
+setup_wget <- function() {
+    has_wget <- yulab.utils:::has_bin("wget")
+
+    if (has_wget) return()
+
+    os <- yulab.utils:::which_os()
+    if (os != "Windows") {
+        # we assume `wget` is installed in MacOS and Linux
+        stop("Please install wget manually")
+    }
+
+    cat("wget is not found. Download it?\n")
+    if (utils::menu(c("Yes", "No")) != 1) {
+        stop("Exit.")
+    } 
+    
+    url <- "https://eternallybored.org/misc/wget/1.21.4/32/wget.exe"
+    dir <- yulab.utils:::user_dir("wget", appauthor="YuLab")
+    destfile <- sprintf("%s/wget.exe", dir) 
+  
+    yulab.utils:::mydownload(url, destfile)
+
+    PATH <- Sys.getenv('path')
+    Sys.setenv(path = paste0(destfile, ';', PATH))
+}
+
--- R/zzz.R
+++ R/zzz.R
@@ -1,12 +1,5 @@
 .onAttach <- function(libname, pkgname) {
-    os <- Sys.info()[1]
-    if (os == "Windows") {
-        ## windows binary file downloaded from https://eternallybored.org/misc/wget/
-        path <- Sys.getenv('path')
-        wget_path <- system.file("win", package="wget")
-        
-        Sys.setenv(path = paste0(wget_path, ';', path))
-    }
+    setup_wget()
     get_download_setting()
     wget_set()
 }
